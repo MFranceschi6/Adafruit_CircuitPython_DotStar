82926ef0758e7475002dfafa00cd6cfac9aa3176
Matteo Franceschi <mfranceschi94@gmail.com>
2019-01-24 21:31:48 +0100

    update dotstar to handle multiple lines

--- adafruit_dotstar.py
+++ adafruit_dotstar.py
@@ -261,8 +261,7 @@ class DotStar:
         s += "Start header size -> " + str(START_HEADER_SIZE) + '\n'
         s += "End header index -> " + str(self.end_header_index) + '\n'
         s += 'End header size -> ' + str(self.end_header_size) + '\n'
-        s += '| header --------'
-        for i in range(self._n):
+        for i in range(self._n * 4 + START_HEADER_SIZE + self.end_header_size):
             s += ' ' + str(i) + "  "
         s += ' end header |\n'
         for i in range(self._line):
